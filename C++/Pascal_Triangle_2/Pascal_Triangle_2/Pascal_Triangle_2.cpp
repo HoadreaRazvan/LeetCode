9f2acd3011f4eae2c387a8f4dbefa27b55cbe325
HoadreaRazvan <162281824+HoadreaRazvan@users.noreply.github.com>
2024-09-10 16:25:42 +0300

    Pascal's Triangle 2 more efficient

--- C++/Pascal_Triangle_2/Pascal_Triangle_2/Pascal_Triangle_2.cpp
+++ C++/Pascal_Triangle_2/Pascal_Triangle_2/Pascal_Triangle_2.cpp
@@ -36,17 +36,14 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> getRow(int rowIndex) {
-        vector<vector<int>> triangle(rowIndex+1);
-        for (int i = 0; i <= rowIndex; i++) {
-            triangle[i].resize(i + 1);
-            triangle[i][0] = 1;
-            triangle[i][i] = 1;
-            for (int j = 1; j < i; j++) {
-                triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+    std::vector<int> getRow(int rowIndex) {
+        vector<int> row(rowIndex + 1, 1);
+        for (int i = 2; i <= rowIndex; ++i) {
+            for (int j = i - 1; j > 0; --j) {
+                row[j] += row[j - 1];
             }
         }
-        return triangle[rowIndex];
+        return row;
     }
 };
 
